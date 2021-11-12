5654f3e644dd987540501a415bda09768e001d2d
agilan2001 <agilanvlr2001@gmail.com>
2021-11-12 14:16:47 +0530

    clean code

--- code/main.py
+++ code/main.py
@@ -7,9 +7,6 @@ from keras.applications import imagenet_utils
 
 mobile = MobileNet()
 def prepare_image(img):
-    # img_path = ''
-    # img = image.load_img(img_path + file, target_size=(224, 224))
-    # img_array = image.img_to_array(img)
     img_resize = cv2.resize(img,(224,224))
     img_array_expanded_dims = np.expand_dims(img_resize, axis=0)
     return preprocess_input(img_array_expanded_dims)
@@ -21,8 +18,8 @@ bg_frame_cnt = bg_frame_end-bg_frame_start+1
 img_frame_start = 0
 img_frame_end = 144
 
-frame_width = 640 #1280
-frame_height = 360 #720
+frame_width = 640 
+frame_height = 360 
 
 cap = cv2.VideoCapture(r"src/final2.mp4")
 
@@ -37,9 +34,7 @@ cap.set(1, bg_frame_start)
 bg_avg = np.zeros((frame_height,frame_width,3), dtype="uint8")
 
 while True:
-    # Capture frame-by-frame
     ret, frame = cap.read()
-    # if frame is read correctly ret is True
     if not ret:
         break
 
@@ -51,42 +46,37 @@ while True:
     if(cnt==bg_frame_cnt):
         break
     
+cv2.imshow("Background_Avg", bg_avg)
+cv2.imwrite("results/Back_Avg.png", bg_avg)
+cv2.waitKey()
+cv2.destroyAllWindows()
     
 
 cap.set(img_frame_start,0)
 cnt = 0
-# res = True
 
 while True:
-    # Capture frame-by-frame
+    
     ret, frame = cap.read()
-    # if frame is read correctly ret is True
     if not ret:
         break
 
     frame = cv2.resize(frame, (frame_width, frame_height))
 
-    # if cnt==0:
-    #     cv2.imshow("Frame", frame)
-    #     cv2.imwrite("results/Frame.png", frame)
+    cv2.imshow("Video_Feed", frame)
     
-    
-    # print(cnt, frame[0][0] - [1, 2, 3])
+
     subt = cv2.subtract(frame,bg_avg)
     blur = cv2.GaussianBlur(subt,(5,5),3)
     edged = cv2.Canny(blur, 250, 250) 
     (contours, _) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # contours = imutils.grab_contours(contours)
-    # print(contours)
     contours = sorted(contours, key = cv2.contourArea, reverse = True)[:20]
-    # print( contours)
-    # break
 
     x_min = frame_width
     y_min = frame_height
     x_max = 0
     y_max = 0
-                                                                    #contours: [contour[segment[line [ point [x1,y1]] ], [] ], [], []]
+   
     for x in contours:
         for y in x:
             for z in y:
@@ -116,26 +106,11 @@ while True:
         cv2.drawContours(frame, contours, -1, (0, 0, 255), 3)
         cv2.rectangle(frame,(x_min, y_min),(x_max, y_max),(0,255,0),3)
         cv2.putText(frame, f"Tiger : {conf[0]*100:.2f}%", (x_min,y_min-5), cv2.FONT_HERSHEY_PLAIN, 1.5, (0,255,255), 2)
-        # if conf[0]>0.9 and res:
-        #     cv2.imwrite('results/MobileNet.png',frame)
-        #     res=False
 
+    cv2.imshow("Background_subtracted", subt)
+    cv2.imshow("Edges", edged)
 
-    
-    # break
-
-    # if cnt==0:
-    #     cv2.imshow("Background subtracted", subt)
-    #     cv2.imwrite("results/Back_subt.png", subt)
-
-    #     cv2.imshow("Edges", edged)
-    #     cv2.imwrite("results/Edged.png", edged)
-
-    #     cv2.imshow("Bounded", frame)
-    #     cv2.imwrite("results/Bounded.png", frame)
-
-
-    cv2.imshow(f"frame",frame)
+    cv2.imshow("Bounds",frame)
     
     cv2.waitKey(10)
 
