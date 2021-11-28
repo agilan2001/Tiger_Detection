9691fa994dad2b673d0f8fc17dc62e805db5dc7d
agilan2001 <agilanvlr2001@gmail.com>
2021-11-28 20:17:42 +0530

    create main loop

--- code/test.py
+++ code/test.py
@@ -11,13 +11,17 @@ def prepare_image(img):
     img_array_expanded_dims = np.expand_dims(img_resize, axis=0)
     return preprocess_input(img_array_expanded_dims)
 
-bg_frame_cnt = 10
-frame_cnt = 30*60
+bg_frame_start = 145
+bg_frame_end = 165
+bg_frame_cnt = bg_frame_end-bg_frame_start+1
+
+img_frame_start = 0
+img_frame_end = 144
 
 frame_width = 640 
 frame_height = 360 
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(r"src/final2.mp4")
 
 
 if not cap.isOpened():
@@ -25,27 +29,42 @@ if not cap.isOpened():
     exit()
 
 cnt = 0
+cap.set(1, bg_frame_start)
 
 bg_avg = np.zeros((frame_height,frame_width,3), dtype="uint8")
 
 while True:
-    cnt += 1
-    print(cnt)
-    if(cnt==frame_cnt): 
-        cnt = 0
-        bg_avg = np.zeros((frame_height,frame_width,3), dtype="uint8")
-
     ret, frame = cap.read()
     if not ret:
         break
 
     frame = cv2.resize(frame, (frame_width, frame_height))
+    
+    bg_avg+=(frame//bg_frame_cnt)
+    
+    cnt += 1
+    if(cnt==bg_frame_cnt):
+        break
+    
+cv2.imshow("Background_Avg", bg_avg)
+cv2.imwrite("results/Back_Avg.png", bg_avg)
+cv2.waitKey()
+cv2.destroyAllWindows()
+    
+
+cap.set(img_frame_start,0)
+cnt = 0
+
+while True:
+    
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame = cv2.resize(frame, (frame_width, frame_height))
+
     cv2.imshow("Video_Feed", frame)
     
-    if(cnt<bg_frame_cnt) :
-        bg_avg+=(frame//bg_frame_cnt)
-        cv2.imshow("Back_AVG", bg_avg)
-        continue
 
     subt = cv2.subtract(frame,bg_avg)
     blur = cv2.GaussianBlur(subt,(5,5),3)
@@ -75,18 +94,18 @@ while True:
     y_max = min(y_max + inc, 720)
     y_min = max(y_min - inc, 0)
 
-    if (not(x_max <= x_min or y_max <= y_min)): # if image is empty
-        # continue
+    if (x_max <= x_min or y_max <= y_min): # if image is empty
+        continue
 
-        preprocessed_image = prepare_image(frame[y_min:y_max, x_min:x_max])
-        predictions = mobile.predict(preprocessed_image)
-        results = imagenet_utils.decode_predictions(predictions)
-        conf = [i[2] for i in results[0] if 'tiger' in i[1]]
-        
-        if conf:
-            cv2.drawContours(frame, contours, -1, (0, 0, 255), 3)
-            cv2.rectangle(frame,(x_min, y_min),(x_max, y_max),(0,255,0),3)
-            cv2.putText(frame, f"Tiger : {conf[0]*100:.2f}%", (x_min,y_min-5), cv2.FONT_HERSHEY_PLAIN, 1.5, (0,255,255), 2)
+    preprocessed_image = prepare_image(frame[y_min:y_max, x_min:x_max])
+    predictions = mobile.predict(preprocessed_image)
+    results = imagenet_utils.decode_predictions(predictions)
+    conf = [i[2] for i in results[0] if 'tiger' in i[1]]
+    
+    if conf:
+        cv2.drawContours(frame, contours, -1, (0, 0, 255), 3)
+        cv2.rectangle(frame,(x_min, y_min),(x_max, y_max),(0,255,0),3)
+        cv2.putText(frame, f"Tiger : {conf[0]*100:.2f}%", (x_min,y_min-5), cv2.FONT_HERSHEY_PLAIN, 1.5, (0,255,255), 2)
 
     cv2.imshow("Background_subtracted", subt)
     cv2.imshow("Edges", edged)
@@ -95,6 +114,9 @@ while True:
     
     cv2.waitKey(10)
 
+    cnt += 1
+    if(cnt==img_frame_end): 
+        break
     
 cap.release()
 cv2.destroyAllWindows()
